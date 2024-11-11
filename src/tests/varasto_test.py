3c996feeede0e62c16e1e058e7c09377b536ba4c
Olivia <olivi.kiljala@gmail.com>
2024-11-11 14:59:12 +0200

    päivitettiin testit 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,28 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+        #Oma
+
+    def test_ottaa_liikaa(self):
+        self.varasto.ota_varastosta(11)
+
+    def testa_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(9)
+
+    def test_lisataan_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-3)
+    
+    def test_ota_negatiivinenmaara(self):
+        self.varasto.ota_varastosta(-5)
+
+    def test_taynna(self):
+        self.varasto.lisaa_varastoon(13)
+
+    def test_tilavuus_allenolla(self):
+        self.varasto = Varasto(10, -3)
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_paljonko_tilaa(self):
+        self.uusivarasto = Varasto(10, 5)
+        self.assertEqual(str(self.uusivarasto), "saldo = 5, vielä tilaa 5" )
